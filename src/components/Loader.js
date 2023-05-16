4f75c1547ab700898f39e48500a5b9f8b0905c1a
benitez jose <josebenitezdev@gmail.com>
2023-05-16 18:10:36 -0300

    se crean los archivos de componentes necesarios

---
+++ src/components/Loader.js
@@ -0,0 +1,15 @@
+import { Waveform } from '@uiball/loaders'
+
+export const Loader = () => {
+    return (
+        <div className='container-loader'>
+            <Waveform
+            size={40}
+            lineWeight={3.5}
+            speed={1}
+            color="black"
+        />
+        </div>
+        
+    )
+}
