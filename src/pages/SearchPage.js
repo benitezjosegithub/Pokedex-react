98336f47a38ba283f8fc6513bb99ee0bf6b4332f
benitez jose <josebenitezdev@gmail.com>
2023-05-16 19:30:04 -0300

    se crea pagina de búsqueda de pokemon

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,7 +1,23 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { PokemonContext } from '../context/PokemonContext'
+import { useLocation } from 'react-router-dom';
+import { CardPokemon } from '../components';
 
 export const SearchPage = () => {
+  const location = useLocation();
+  const {allPokemons} = useContext(PokemonContext);
+  const filterPokemons =allPokemons.filter(pokemon=>pokemon.name.includes(location.state.toLowerCase()));
   return (
-    <div>SearchPage</div>
+    <div className='container'>
+      <p className='p-search'>
+        <span>{filterPokemons.length}</span> results were found
+      </p>
+      
+      <div className='card-list-pokemon container'>
+				{filterPokemons.map(pokemon => (
+					<CardPokemon pokemon={pokemon} key={pokemon.id} />
+				))}
+			</div>
+    </div>
   )
 }
