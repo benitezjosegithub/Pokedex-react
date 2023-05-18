93bc94596a7cb0e5dbaf9e68679d7f3831ea515a
benitez jose <josebenitezdev@gmail.com>
2023-05-18 16:03:17 -0300

    se crea nuevo boton de filtro y se agrega README

--- src/components/FilterBar.js
+++ src/components/FilterBar.js
@@ -1,33 +1,29 @@
-import React, { useContext, useEffect, useState } from 'react'
+import React, { useContext } from 'react'
 import { PokemonContext } from '../context/PokemonContext';
-import './css/FilterBar.css'
 
 export const FilterBar = () => {
 
-    const {active,allType,handleCheckbox} = useContext(PokemonContext);
+    const { allType, handleCheckbox } = useContext(PokemonContext);
 
-    // const [allType,setAllType]=useState([]);
-    const [loading, setLoading] = useState(true);
+    return (
+        <div>
+            <div className="container-filters">
+                <div className='filter-by typre'>
+                    <span>Type</span>
 
-  return (
-    <div>
-        <div className={`container-filters ${active?'active':''}`}>
-            <div className='filter-by typre'>
-                <span>Type</span>
-
-                {allType.map(type=>(
-                    <div className='group-type'>
-					<input
-						type='checkbox'
-						onChange={handleCheckbox}
-						name={type.name}
-						id={type.name}
-					/>
-					<label htmlFor={type.name}>{type.name}</label>
-				</div>)
-                )}
+                    {allType.map(type => (
+                        <div className='group-type'>
+                            <input
+                                type='checkbox'
+                                onChange={handleCheckbox}
+                                name={type.name}
+                                id={type.name}
+                            />
+                            <label htmlFor={type.name}>{type.name}</label>
+                        </div>)
+                    )}
+                </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
